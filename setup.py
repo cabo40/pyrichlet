8444f8f0105ab5368c6d84cbe75f23487457c390
cabo <cfso100@gmail.com>
2021-01-05 21:02:37 -0600

    add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrichlet-cabo40", # Replace with your own username
+    version="0.0.1",
+    author="Fidel Selva",
+    author_email="cfso100@gmail.com",
+    description="A package for density estimation and clustering using infinite gaussian mixtures",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cabo40/pyrichlet",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
