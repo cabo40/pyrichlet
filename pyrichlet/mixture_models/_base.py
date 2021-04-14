86f2334b2cc78e1eb4987adf7f6955b4ccd2f29e
cabo40 <cfso100@gmail.com>
2021-04-14 14:30:53 -0500

    style: refactorization of calls and names

--- pyrichlet/mixture_models/_base.py
+++ pyrichlet/mixture_models/_base.py
@@ -1,3 +1,4 @@
+from sklearn.cluster import SpectralClustering
 from scipy.stats import multivariate_normal
 import pandas as pd
 import numpy as np
@@ -40,7 +41,7 @@ class BaseGaussianMixture(metaclass=ABCMeta):
 
         self.map_sim_params = None
         self.map_log_likelihood = -np.inf
-        self.n_len = 0
+        self.total_saved_steps = 0
         self.sim_params = []
         self.n_groups = []
         self.n_atoms = []
@@ -63,27 +64,23 @@ class BaseGaussianMixture(metaclass=ABCMeta):
             self.psi_prior = np.atleast_2d(np.cov(self.y.T))
         if self.nu_prior is None:
             _, self.nu_prior = self.y.shape
+        self.mu = self.mu.reshape(0, *self.mu_prior.shape)
+        self.sigma = self.sigma.reshape(0, *self.psi_prior.shape)
 
         if not warm_start:
             self.sim_params = []
             self.n_groups = []
             self.n_atoms = []
-            self.n_len = 0
+            self.total_saved_steps = 0
 
             self.affinity_matrix = np.zeros((len(self.y), len(self.y)))
 
-            self.u = self.rng.uniform(0 + np.finfo(np.float64).eps, 1, len(self.y))
-            self.mu, self.sigma = _utils.random_normal_invw(
-                mu=self.mu_prior,
-                lam=self.lambda_prior,
-                psi=self.psi_prior,
-                nu=self.nu_prior,
-                rng=self.rng)
-            self.mu = np.array([self.mu])
-            self.sigma = np.array([self.sigma])
-
+            self.u = self.rng.uniform(0 + np.finfo(np.float64).eps, 1,
+                                      len(self.y))
             self.weight_model.tail(1 - min(self.u))
+
             self.d = self.weight_model.random_assignment(len(self.y))
+
             self._complete_atoms()
         self._train_gibbs()
 
@@ -99,12 +96,14 @@ class BaseGaussianMixture(metaclass=ABCMeta):
             self.psi_prior = np.atleast_2d(np.cov(self.y.T))
         if self.nu_prior is None:
             _, self.nu_prior = self.y.shape
+        self.mu = self.mu.reshape(0, *self.mu_prior.shape)
+        self.sigma = self.sigma.reshape(0, *self.psi_prior.shape)
 
         if not warm_start:
             self.sim_params = []
             self.n_groups = []
             self.n_atoms = []
-            self.n_len = 0
+            self.total_saved_steps = 0
 
             self.affinity_matrix = np.zeros((len(self.y), len(self.y)))
 
@@ -149,19 +148,44 @@ class BaseGaussianMixture(metaclass=ABCMeta):
                                                     param["u"]))
         return np.array(y_sim).mean(axis=0)
 
-    def gibbs_map_density(self, y):
+    def gibbs_eap_affinity_matrix(self, y=None):
+        if y is None:
+            return self.affinity_matrix / self.total_saved_steps
+        affinity_matrix = np.zeros((len(y), len(y)))
+        for params in self.sim_params:
+            grouping = _utils.cluster(y,
+                                      params["w"],
+                                      params["mu"],
+                                      params["sigma"],
+                                      params["u"])
+            affinity_matrix += np.equal(grouping, grouping[:, None])
+        affinity_matrix /= len(self.sim_params)
+        return affinity_matrix
+
+    def gibbs_eap_spectral_consensus_cluster(self, y=None, n_clusters=1):
+        sc = SpectralClustering(n_clusters=n_clusters, affinity='precomputed')
+        return sc.fit_predict(self.gibbs_eap_affinity_matrix(y))
+
+    def gibbs_map_density(self, y=None):
+        if y is None:
+            y = self.y
         return _utils.mixture_density(y,
                                       self.map_sim_params["w"],
                                       self.map_sim_params["mu"],
                                       self.map_sim_params["sigma"],
                                       self.map_sim_params["u"])
 
-    def gibbs_map_cluster(self, y):
-        return _utils.cluster(y,
-                              self.map_sim_params["w"],
-                              self.map_sim_params["mu"],
-                              self.map_sim_params["sigma"],
-                              self.map_sim_params["u"])
+    def gibbs_map_cluster(self, y=None, full=False):
+        if y is None:
+            y = self.y
+        ret = _utils.cluster(y,
+                             self.map_sim_params["w"],
+                             self.map_sim_params["mu"],
+                             self.map_sim_params["sigma"],
+                             self.map_sim_params["u"])
+        if full:
+            return ret
+        return ret[0]
 
     def get_n_groups(self):
         return self.n_groups
@@ -185,7 +209,7 @@ class BaseGaussianMixture(metaclass=ABCMeta):
         self.n_atoms.append(len(self.mu))
         self._update_map_params()
         self.affinity_matrix += np.equal(self.d, self.d[:, None])
-        self.n_len += 1
+        self.total_saved_steps += 1
 
     def _update_map_params(self):
         run_log_likelihood = self._full_log_likelihood()
@@ -219,8 +243,8 @@ class BaseGaussianMixture(metaclass=ABCMeta):
             self.mu.append(temp_mu)
             self.sigma.append(temp_sigma)
 
-        self.mu = np.array(self.mu)
-        self.sigma = np.array(self.sigma)
+        self.mu = np.array(self.mu).reshape(-1, *self.mu_prior.shape)
+        self.sigma = np.array(self.sigma).reshape(-1, *self.psi_prior.shape)
 
     def _complete_atoms(self):
         missing_len = self.weight_model.get_size() - len(self.mu)
@@ -232,8 +256,9 @@ class BaseGaussianMixture(metaclass=ABCMeta):
                 nu=self.nu_prior,
                 rng=self.rng
             )
-            self.mu = np.concatenate((self.mu, [temp_mu]))
-            self.sigma = np.concatenate((self.sigma, [temp_sigma]))
+            self.mu = np.concatenate((self.mu, [np.atleast_1d(temp_mu)]))
+            self.sigma = np.concatenate((self.sigma,
+                                         [np.atleast_2d(temp_sigma)]))
 
     def _update_d(self):
         logproba = self._d_log_likelihood_vector()
@@ -290,7 +315,6 @@ class BaseGaussianMixture(metaclass=ABCMeta):
                 self._m_step()
                 if i % self.subsample_steps == 0:
                     self._save_params()
-
 
     def _d_log_likelihood_vector(self):
         with np.errstate(divide='ignore'):
--- pyrichlet/mixture_models/_geometric_process_mixture.py
+++ pyrichlet/mixture_models/_geometric_process_mixture.py
@@ -3,15 +3,9 @@ from ..weight_models import GeometricProcess
 
 
 class GeometricProcessMixture(BaseGaussianMixture):
-    def __init__(self, *, a=1, b=1, mu_prior=None,
-                 lambda_prior=1, psi_prior=None, nu_prior=None, total_iter=1000,
-                 burn_in=100, subsample_steps=1, rng=None):
+    def __init__(self, *, a=1, b=1, rng=None, **kwargs):
         weight_model = GeometricProcess(a=a, b=b, rng=rng)
-        super().__init__(weight_model=weight_model, mu_prior=mu_prior,
-                         lambda_prior=lambda_prior, psi_prior=psi_prior,
-                         nu_prior=nu_prior, total_iter=total_iter,
-                         burn_in=burn_in, subsample_steps=subsample_steps,
-                         rng=rng)
+        super().__init__(weight_model=weight_model, rng=rng, **kwargs)
 
     def _get_run_params(self):
         return {"w": self.weight_model.get_weights(),
