92579da72d2aae2d2b3f26107d4dc12c0f8ca8e6
cabo40 <cfso100@gmail.com>
2021-09-13 18:54:29 -0500

    BetaInBeta: add variational methods

--- pyrichlet/weight_models/_beta_in_beta.py
+++ pyrichlet/weight_models/_beta_in_beta.py
@@ -1,8 +1,13 @@
+from scipy.special import loggamma
+
 from ._base import BaseWeight
 import numpy as np
 from scipy.stats import beta
 from scipy.optimize import minimize, brentq
 from scipy.integrate import quad
+
+from ..exceptions import NotFittedError
+from ..utils.functions import mean_log_beta
 
 
 class BetaInBeta(BaseWeight):
@@ -215,3 +220,101 @@ class BetaInBeta(BaseWeight):
                             "inverse-sampling"]
         if self.p_method not in accepted_methods:
             raise ValueError(f"p_method must be one of {accepted_methods}")
+
+    def fit_variational(self, variational_d):
+        self.variational_d = variational_d
+        self.variational_k = len(self.variational_d)
+        self.variational_params = np.empty((self.variational_k, 2),
+                                           dtype=np.float64)
+        a_c = np.sum(self.variational_d, 1)
+        b_c = np.concatenate((np.cumsum(a_c[::-1])[-2::-1], [0]))
+        self.variational_params[:, 0] = 1 + self.x / (
+                1 - self.x) * self.p + a_c
+        self.variational_params[:, 1] = self.alpha + self.x / (
+                1 - self.x) * (1 - self.p) + b_c
+
+    def variational_mean_log_w_j(self, j):
+        if self.variational_d is None:
+            raise NotFittedError
+        res = 0
+        for jj in range(j):
+            res += mean_log_beta(self.variational_params[jj][1],
+                                 self.variational_params[jj][0])
+        res += mean_log_beta(self.variational_params[j, 0],
+                             self.variational_params[j, 1]
+                             )
+        return res
+
+    def variational_mean_log_p_d__w(self, variational_d=None):
+        if variational_d is None:
+            _variational_d = self.variational_d
+            if _variational_d is None:
+                raise NotFittedError
+        else:
+            _variational_d = variational_d
+        res = 0
+        for j, nj in enumerate(np.sum(_variational_d, 1)):
+            res += nj * self.variational_mean_log_w_j(j)
+        return res
+
+    def variational_mean_log_p_w(self):
+        if self.variational_d is None:
+            raise NotFittedError
+        res = 0
+        for j, params in enumerate(self.variational_params):
+            res += mean_log_beta(params[0], params[1]) * self.x / (
+                    1 - self.x) * self.p
+            res += mean_log_beta(params[1], params[0]) * (
+                    self.alpha + self.x / (1 - self.x) * (1 - self.p) - 1
+            )
+            res += loggamma(1 + self.x / (1 - self.x) + self.alpha)
+            res -= loggamma(1 + self.x / (1 - self.x) * self.p)
+            res -= loggamma(self.alpha + self.x / (1 - self.x) * (1 - self.p))
+        return res
+
+    def variational_mean_log_q_w(self):
+        if self.variational_d is None:
+            raise NotFittedError
+        res = 0
+        for params in self.variational_params:
+            res += (params[0] - 1) * mean_log_beta(params[0], params[1])
+            res += (params[1] - 1) * mean_log_beta(params[1], params[0])
+            res += loggamma(params[0] + params[1])
+            res -= loggamma(params[0]) + loggamma(params[1])
+        return res
+
+    def variational_mean_w(self, j):
+        if j > self.variational_k:
+            return 0
+        res = 1
+        for jj in range(j):
+            res *= (self.variational_params[jj][1] /
+                    self.variational_params[jj].sum())
+        res *= self.variational_params[j, 0] / self.variational_params[j].sum()
+        return res
+
+    def variational_mode_w(self, j):
+        if j > self.variational_k:
+            return 0
+        res = 1
+        for jj in range(j):
+            if self.variational_params[jj, 1] <= 1:
+                if self.variational_params[jj, 0] <= 1:
+                    raise ValueError('multimodal distribution')
+                else:
+                    return 0
+            elif self.variational_params[jj, 0] <= 1:
+                continue
+            res *= ((self.variational_params[jj, 1] - 1) /
+                    (self.variational_params[jj].sum() - 2))
+
+        if self.variational_params[j, 0] <= 1:
+            if self.variational_params[j, 1] <= 1:
+                raise ValueError('multimodal distribution')
+            else:
+                return 0
+        elif self.variational_params[j, 1] <= 1:
+            return res
+        res *= ((self.variational_params[j, 0] - 1) /
+                (self.variational_params[j].sum() - 2))
+        return res
