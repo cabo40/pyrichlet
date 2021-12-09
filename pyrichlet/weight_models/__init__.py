a37d70f8dc9e978e88eefc5f3c299394fb92bcb7
cabo40 <cfso100@gmail.com>
2021-12-09 16:23:55 -0600

    weight_models: add BetaInDirichlet to weight_models imports

--- pyrichlet/weight_models/__init__.py
+++ pyrichlet/weight_models/__init__.py
@@ -3,8 +3,9 @@ from ._dirichlet_distribution import DirichletDistribution
 from ._dirichlet_process import DirichletProcess
 from ._pitman_yor_process import PitmanYorProcess
 from ._geometric_process import GeometricProcess
+from ._beta_in_dirichlet import BetaInDirichlet
 from ._beta_in_beta import BetaInBeta
 from ._beta_bernoulli import BetaBernoulli
 from ._beta_binomial import BetaBinomial
-from ._frequency import FrequencyWeighting
 from ._equal import EqualWeighting
+from ._frequency import FrequencyWeighting
