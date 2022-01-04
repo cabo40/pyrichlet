ab26ff5ee67ecbed7999d97634123a487b946405
cabo40 <cfso100@gmail.com>
2022-01-04 14:43:56 -0600

    ReadTheDocs don't fail on warning

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.append(os.path.abspath("")[:-4])
+sys.path.insert(0, os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 
