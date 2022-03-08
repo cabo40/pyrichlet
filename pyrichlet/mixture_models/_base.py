Reimplement `k-means` initialization and fix arising bugs in geometric, frequency and equal weighting.

pyrichlet/mixture_models/_base.py
@@ -606,37 +606,13 @@ class BaseGaussianMixture(metaclass=ABCMeta):
                 random_state=np.random.RandomState(self.rng.bit_generator)
             )
             d = km.fit_predict(self.y)
-            dim = self.y.shape[1]
             var_d = np.zeros((self.var_k, self.y.shape[0]),
                              dtype=np.float64)
-            for j in range(self.var_k):
-                y_subset = self.y[d == j]
-                if len(y_subset) == 0:
-                    var_d[j, :] = np.finfo(np.float64).eps
-                    continue
-                mu_j = np.mean(y_subset, 0)
-                precisions_j = np.linalg.inv(
-                    np.atleast_2d(np.cov(y_subset.T))
-                )
-                mu_j = np.atleast_1d(mu_j)
-                precisions_j = np.atleast_2d(precisions_j)
-                # atoms initialization
-                self.var_theta.append([mu_j, self.lambda_prior,
-                                       precisions_j, self.nu_prior])
-                # assignations initialization
-                log_d_ji = _utils.e_log_norm_wishart(precisions_j,
-                                                     self.nu_prior) / 2
-                log_d_ji -= dim / (2 * self.lambda_prior)
-                log_d_ji -= (self.nu_prior / 2 *
-                             ((self.y - mu_j).T * (precisions_j @ (
-                                     self.y - mu_j).T)).sum(0)
-                             )
-                var_d[j, :] = log_d_ji
-            var_d -= var_d.max(0)
-            var_d = np.exp(var_d)
-            var_d += np.finfo(np.float64).eps
-            var_d /= var_d.sum(0)
+            var_d[d, range(len(d))] = 1
             self.var_d = var_d
+            self.weight_model.fit_variational(np.empty(shape=(self.var_k, 0)))
+            self._update_var_theta()
+            self._update_var_d()
         elif method == "random":
             for _ in range(self.var_k):
                 mu_j, temp_psi = _utils.random_normal_invw(

pyrichlet/weight_models/_equal.py
@@ -18,36 +18,28 @@ class EqualWeighting(BaseWeight):
 
     def fit_variational(self, variational_d: np.ndarray):
         self.variational_d = variational_d
-        self.n = self.variational_d.shape[1]
+        self.n = self.variational_d.shape[0]
+        self.variational_k = self.n
 
     def variational_mean_log_w_j(self, j):
-        if self.variational_d is None:
-            raise NotFittedError
-        return 0
+        return np.log(1 / self.n) * (j < self.n)
 
     def variational_mean_log_p_d__w(self, variational_d=None):
-        if variational_d is None:
+        _variational_d = variational_d
+        if _variational_d is None:
             _variational_d = self.variational_d
             if _variational_d is None:
                 raise NotFittedError
-        return 0
+        return _variational_d.shape[1] * np.log(self.n)
 
     def variational_mean_log_p_w(self):
-        if self.variational_d is None:
-            raise NotFittedError
         return 0
 
     def variational_mean_log_q_w(self):
-        if self.variational_d is None:
-            raise NotFittedError
         return 0
 
     def variational_mean_w(self, j):
-        if j >= self.variational_k:
-            return 0
-        return 1 / self.n
+        return 1 / self.n * (j < self.n)
 
     def variational_mode_w(self, j):
-        if j >= self.variational_k:
-            return 0
-        return 1 / self.n
+        return 1 / self.n * (j < self.n)

pyrichlet/weight_models/_frequency.py
@@ -23,13 +23,17 @@ class FrequencyWeighting(BaseWeight):
     def fit_variational(self, variational_d: np.ndarray):
         self.variational_d = variational_d
         self.variational_k = variational_d.shape[1]
+        if self.variational_k == 0:
+            self.variational_k = self.n
 
     def variational_mean_log_w_j(self, j):
         if self.variational_d is None:
             raise NotFittedError
         if j >= self.variational_k:
             return -np.inf
-        return self.variational_d.sum(1)[j] / self.variational_d.sum()
+        if self.variational_d.shape[1]:
+            return self.variational_d.sum(1)[j] / self.variational_d.sum()
+        return np.log(1 / self.variational_k)
 
     def variational_mean_log_p_d__w(self, variational_d=None):
         if variational_d is None:

pyrichlet/weight_models/_geometric_process.py
@@ -54,7 +54,8 @@ class GeometricProcess(BaseWeight):
         self.variational_d = variational_d
         self.variational_k = len(self.variational_d)
         self.variational_params = np.empty(2, dtype=np.float64)
-        self.variational_params[0] = self.a + len(self.variational_d[0]) - 1
+        self.variational_params[0] = self.a + max(len(self.variational_d[0]),
+                                                  1) - 1
         self.variational_params[1] = (
                 self.b + (self.variational_d[1:].T *
                           range(1, self.variational_k)).sum()