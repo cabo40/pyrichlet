f31ab00d19be58d47d42d4172aac5c615620d6c7
cabo <cfso100@gmail.com>
2023-01-20 12:25:21 -0600

    pyrichlet: drop support of python 3.6. Fix bug in data loaders load_chaetocnema() and load_penguins()

--- pyrichlet/utils/_loaders.py
+++ pyrichlet/utils/_loaders.py
@@ -1,20 +1,17 @@
-try:
-    import importlib.resources as pkg_resources
-except ImportError:
-    import importlib_resources as pkg_resources
+from importlib.resources import files
 import pandas as pd
 from . import _data
 
 
 def load_chaetocnema():
-    df = pd.read_csv(pkg_resources.resource_string(_data, "chaetocnema.csv"),
+    df = pd.read_csv(str(files(_data) / 'chaetocnema.csv'),
                      usecols=list(range(8)),
                      index_col=0)
     return df.iloc[:, :-1], df.iloc[:, -1].to_numpy()
 
 
 def load_penguins():
-    df = pd.read_csv(pkg_resources.resource_string(_data, "penguins.csv"),
+    df = pd.read_csv(str(files(_data) / 'penguins.csv'),
                      usecols=[0, 2, 3, 4, 5])
     df = df.dropna()
     return df.iloc[:, 1:], df.iloc[:, 0].to_numpy()
